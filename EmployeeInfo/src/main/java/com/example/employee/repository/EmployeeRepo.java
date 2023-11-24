9b8c8d3169ecd0d2dc50375f8a12f45dcbb33015
Kankshini Shamkuwar <84704527+kankshini2000@users.noreply.github.com>
2023-11-24 22:07:48 +0530

    Delete EmployeeInfo directory

--- EmployeeInfo/src/main/java/com/example/employee/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.employee.model.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Long>{
-
-}
