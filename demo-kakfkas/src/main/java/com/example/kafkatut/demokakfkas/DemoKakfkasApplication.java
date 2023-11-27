0ebae772dabd44acaaaea0d4082eaf142efc25c2
Kankshini <skankshii@gmail.com>
2023-11-27 23:19:25 +0530

    configured with declarations

---
+++ demo-kakfkas/src/main/java/com/example/kafkatut/demokakfkas/DemoKakfkasApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkatut.demokakfkas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKakfkasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKakfkasApplication.class, args);
+	}
+
+}
---
+++ demo-kakfkas/src/test/java/com/example/kafkatut/demokakfkas/DemoKakfkasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkatut.demokakfkas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKakfkasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
