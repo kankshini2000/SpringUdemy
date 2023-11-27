a4d138a1c5129455a748d2d53e539865d8a68e97
Kankshini <skankshii@gmail.com>
2023-11-28 03:59:00 +0530

    ASYNC communication i.e apache kafka

---
+++ BaseDomains/src/test/java/com/example/BaseDomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaseDomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
